f658af3d68d87ffa858f21c940f59d8c731dce9a
Chris Zichko <chriszichkocoding@gmail.com>
2021-09-05 12:35:28 -0400

    Added options of what type of characters are wanted in password

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 // Assignment code here
 
-// declaring variables
-const lowerUpperabcs = (
-  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-);
-const numberItems = (
-  '0123456789'
-);
-const specialChars = (
-  '!@#$%^&*()-_=+`~[]{}|:;"",<.>/?'
-);
-
-var passwordLength = function() {
-  // save validation for that GitHub issue.
-  let len = prompt("Choose a password length between 8 and 128 characters long");
-  return len;
-};
-
+// Generate password function
 var generatePassword = function(len) {
   let genPass = "";
   len = passwordLength();
+  let lettersNumsSpecs = charTypes();
   // loop through and choose random items
   for (let i = 0; i < len; i++) {
-
-    let lettersNumsSpecs = (
-      lowerUpperabcs + 
-      numberItems + 
-      specialChars
-      );
-
     // Randomly choose which character type
     let passChars = Math.floor(Math.random() * lettersNumsSpecs.length) + 1;
     // Randomly choose a character type and add it to genPass variable
@@ -41,6 +19,52 @@ var generatePassword = function(len) {
   }
   return genPass;
 };
+
+// Determining password length function
+var passwordLength = function() {
+  // save validation for that GitHub issue.
+  let len = prompt("Choose a password length between 8 and 128 characters long");
+  return len;
+};
+
+// Determining character types function
+var charTypes = function() {
+  /* Add validation later */
+
+  //Ask if the user wants lower and upper case letters in their password
+  let letters = confirm("Would you like lower and upper case letters in your password?");
+  if (letters) {
+    var lowerUpperabcs = (
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    );
+  } else {
+    var lowerUpperabcs = "";
+  }
+  
+  // Ask if user wants numbers in their password
+  let nums = confirm("Would you like numbers in your password?");
+  if (nums) {
+    var numberItems = (
+      '0123456789'
+    );
+  } else {
+    var numberItems = "";
+  }
+
+  // Ask if user wants special characters in their password
+  let specs = confirm("Would you like special characters in your password?");
+  if (specs) {
+    var specialChars = (
+      '!@#$%^&*()-_=+`~[]{}|:;"",<.>/?'
+    );    
+  } else {
+    var specialChars = "";
+  }
+  
+  let whatCharTypes = (lowerUpperabcs + numberItems + specialChars)
+
+  return whatCharTypes;
+}
 
 
 // Get references to the #generate element
